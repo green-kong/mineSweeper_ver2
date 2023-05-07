70eb3227f667b9d0e96e849500f869ecfcc8a07f
dev_kong <ahdjd5@gmail.com>
2023-05-07 23:48:05 +0900

    first commit

---
+++ src/main/java/com/example/minesweeper_ver_2/MinesweeperVer2Application.java
@@ -0,0 +1,13 @@
+package com.example.minesweeper_ver_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesweeperVer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minesweeper_ver_2/MinesweeperVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minesweeper_ver_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
